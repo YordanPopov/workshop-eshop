35b9165dc394472d422748228c5cb75b736842d0
YordanPopov <jordanpopov190@gmail.com>
2024-12-09 14:27:03 +0200

    Add negative test for brand lifecycle

--- E2EWorkshop/e2eApiTests/BrandApiTests.cs
+++ E2EWorkshop/e2eApiTests/BrandApiTests.cs
@@ -4,6 +4,7 @@ using RestSharp;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -21,7 +22,7 @@ namespace e2eApiTests
         {
             client = new RestClient(GlobalConstants.BaseUrl);
             adminToken = GlobalConstants.AuthenticateUser("admin@gmail.com", "admin@gmail.com");
-            userToken = GlobalConstants.AuthenticateUser("jordan@gmail.com", "jordan@gmail.com");
+            userToken = GlobalConstants.AuthenticateUser("johndoe@example.com", "password123");
             Assert.That(adminToken, Is.Not.Null.Or.Empty,
                 "Authentication token is null or empty");
             Assert.That(userToken, Is.Not.Null.Or.Empty,
@@ -104,6 +105,53 @@ namespace e2eApiTests
             var cancelOrderResponse = client.Execute(cancelOrderRequest, Method.Put);
             Assert.That(cancelOrderResponse.IsSuccessful, Is.True,
                 "Order cancellation failed");
+        }
+
+        [Test]
+        public void BrandLifecycleNegativeTest()
+        {
+            string invalidToken = "InvalidToken";
+            string invalidId = "IdnvalidId";
+
+            var createBrandRequest = new RestRequest("/brand");
+            createBrandRequest.AddHeader("Authorization", $"Bearer {invalidToken}");
+            createBrandRequest.AddJsonBody(new { Title = "New Brand" });
+
+            var createBrandResponse = client.Execute(createBrandRequest, Method.Post);
+            Assert.That(createBrandResponse.IsSuccessful, Is.False,
+                "Creating Brand is successful");
+            Assert.That(createBrandResponse.StatusCode, Is.EqualTo(HttpStatusCode.InternalServerError),
+                "Response status is not Internal Server Error");
+
+            var getBrandRequest = new RestRequest("/brand/{id}");
+            getBrandRequest.AddUrlSegment("id", invalidId);
+
+            var getBrandResponse = client.Execute(getBrandRequest, Method.Get);
+            Assert.That(getBrandResponse.IsSuccessful, Is.False,
+                "Get Brand is successful");
+            Assert.That(getBrandResponse.StatusCode, Is.EqualTo(HttpStatusCode.InternalServerError),
+                "Status code is not Internal Server Error");
+
+            var updateBrandRequest = new RestRequest("/brand/{id}");
+            updateBrandRequest.AddHeader("Authorization", $"Bearer {invalidToken}");
+            updateBrandRequest.AddUrlSegment("id", invalidId);
+            updateBrandRequest.AddJsonBody(new { Title = "Updated Brand" });
+
+            var updateBrandResponse = client.Execute(updateBrandRequest, Method.Put);
+            Assert.That(updateBrandResponse.IsSuccessful, Is.False,
+                "Updating Brand is successful");
+            Assert.That(updateBrandResponse.StatusCode, Is.EqualTo(HttpStatusCode.InternalServerError),
+                "Status code is not Internal Server Error");
+
+            var deleteBrandRequest = new RestRequest("/brand/{id}");
+            deleteBrandRequest.AddHeader("Authorization", $"Bearer {invalidToken}");
+            deleteBrandRequest.AddUrlSegment("id", invalidId);
+
+            var deleteBrandResponse = client.Execute(deleteBrandRequest, Method.Delete);
+            Assert.That(deleteBrandResponse.IsSuccessful, Is.False,
+                "Deleting Brand is successful");
+            Assert.That(deleteBrandResponse.StatusCode, Is.EqualTo(HttpStatusCode.InternalServerError),
+                "Status code is not Internal Server Error");
         }
 
         public void Dispose()
